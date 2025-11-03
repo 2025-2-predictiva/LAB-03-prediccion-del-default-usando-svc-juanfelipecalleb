a390ca084d7a107997d5cd52f4963dab2deaa190
juanfelipecalleb <jcalleb@unal.edu.co>
2025-11-03 14:53:42 -0500

    LAB 3

--- homework/homework.py
+++ homework/homework.py
@@ -37,6 +37,19 @@
 # clasificación están descritos a continuación.
 #
 #
+import os
+import pandas as pd
+import json
+from sklearn.metrics import confusion_matrix, balanced_accuracy_score, f1_score, precision_score, recall_score
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectKBest
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+
 # Paso 1.
 # Realice la limpieza de los datasets:
 # - Renombre la columna "default payment next month" a "default".
@@ -48,9 +61,31 @@
 # - Remueva la columna "ID".
 #
 #
+
+def load_and_clean(path):
+    df = pd.read_csv(path, compression="zip")
+    df = df.rename(columns = {'default payment next month':'default'})
+    df = df.drop('ID', axis=1)
+    df = df.loc[df["MARRIAGE"] != 0] 
+    df = df.loc[df["EDUCATION"] != 0] 
+    df = df.dropna()
+    df['EDUCATION'] = df['EDUCATION'].map(lambda x: 4 if x>4 else x)
+
+    return df
+
+df_train = load_and_clean('files/input/train_data.csv.zip')
+df_test = load_and_clean('files/input/test_data.csv.zip')
+
 # Paso 2.
 # Divida los datasets en x_train, y_train, x_test, y_test.
 #
+
+x_train = df_train.drop(columns='default')
+y_train = df_train['default']
+
+x_test = df_test.drop(columns='default')
+y_test = df_test['default']
+
 #
 # Paso 3.
 # Cree un pipeline para el modelo de clasificación. Este pipeline debe
@@ -62,17 +97,76 @@
 # - Selecciona las K columnas mas relevantes de la matrix de entrada.
 # - Ajusta una maquina de vectores de soporte (svm).
 #
+
+def make_pipeline():
+
+    categoric_columns = ['SEX','EDUCATION','MARRIAGE']
+
+    other_columns = [col for col in x_train.columns if col not in categoric_columns]
+
+    preprocessor = ColumnTransformer(transformers=[
+        ('onehot', OneHotEncoder(handle_unknown='ignore'), categoric_columns),
+        ("scaler", StandardScaler(with_mean=True, with_std=True), other_columns)
+        ],remainder='passthrough')
+
+
+    pipeline = Pipeline(
+        steps=[
+            ("preprocessor", preprocessor),
+            ('pca', PCA()),
+            ('selectkbest', SelectKBest()),
+            ('svm', SVC(kernel="rbf", random_state=12345, max_iter=-1) )
+        ]
+    )
+
+    return pipeline
+
+pipeline = make_pipeline()
+
 #
 # Paso 4.
 # Optimice los hiperparametros del pipeline usando validación cruzada.
 # Use 10 splits para la validación cruzada. Use la función de precision
 # balanceada para medir la precisión del modelo.
 #
+def make_grid_search(estimator):
+
+    grid_search = GridSearchCV(
+        estimator=estimator,
+        param_grid = {
+            "pca__n_components": [20, x_train.shape[1] - 2],  
+            'selectkbest__k': [12],           
+            'svm__kernel': ["rbf"],           
+            'svm__gamma': [0.1],                  
+        },
+        cv=10,
+        scoring='balanced_accuracy',
+        n_jobs=-1,
+        verbose=3
+    )
+
+    return grid_search
+
+estimator = make_grid_search(pipeline)
+estimator.fit(x_train, y_train)
 #
 # Paso 5.
 # Guarde el modelo (comprimido con gzip) como "files/models/model.pkl.gz".
 # Recuerde que es posible guardar el modelo comprimido usanzo la libreria gzip.
 #
+
+def save_estimator(estimator, path):
+
+    import pickle
+    import gzip
+    import os
+
+    os.makedirs(os.path.dirname(path), exist_ok=True) 
+    with gzip.open(path, "wb") as f:
+        pickle.dump(estimator, f)
+
+save_estimator(estimator,'files/models/model.pkl.gz')
+
 #
 # Paso 6.
 # Calcule las metricas de precision, precision balanceada, recall,
@@ -85,6 +179,32 @@
 # {'dataset': 'train', 'precision': 0.8, 'balanced_accuracy': 0.7, 'recall': 0.9, 'f1_score': 0.85}
 # {'dataset': 'test', 'precision': 0.7, 'balanced_accuracy': 0.6, 'recall': 0.8, 'f1_score': 0.75}
 #
+
+def check_estimator(estimator, x, y, dataset):
+
+    y_pred = estimator.predict(x)
+
+    precision = round(precision_score(y, y_pred), 4)
+    balanced_accuracy = round(balanced_accuracy_score(y, y_pred), 4)
+    f1 = round(f1_score(y, y_pred), 4)
+    recall = round(recall_score(y, y_pred), 4)
+
+    metrics = {
+        "type": "metrics",
+        "dataset": dataset,
+        "precision": precision,
+        "balanced_accuracy": balanced_accuracy,
+        "recall": recall,
+        "f1_score": f1
+    }
+    
+    return metrics, y_pred, y
+
+
+
+metrics_train, y_pred_train, y_train = check_estimator(estimator, x_train, y_train, "train")
+metrics_test, y_pred_test, y_test = check_estimator(estimator, x_test, y_test, "test")
+
 #
 # Paso 7.
 # Calcule las matrices de confusion para los conjuntos de entrenamiento y
@@ -95,3 +215,22 @@
 # {'type': 'cm_matrix', 'dataset': 'train', 'true_0': {"predicted_0": 15562, "predicte_1": 666}, 'true_1': {"predicted_0": 3333, "predicted_1": 1444}}
 # {'type': 'cm_matrix', 'dataset': 'test', 'true_0': {"predicted_0": 15562, "predicte_1": 650}, 'true_1': {"predicted_0": 2490, "predicted_1": 1420}}
 #
+
+def c_matrix(y_true, y_pred, dataset):
+    cm = confusion_matrix(y_true, y_pred)
+    return {
+        "type": "cm_matrix", "dataset": dataset,
+        "true_0": {"predicted_0": int(cm[0, 0]), "predicted_1": int(cm[0, 1])},
+        "true_1": {"predicted_0": int(cm[1, 0]), "predicted_1": int(cm[1, 1])}
+    }
+
+os.makedirs("files/output", exist_ok=True)
+
+c_train = c_matrix(y_train, y_pred_train, "train")
+c_test = c_matrix(y_test, y_pred_test, "test")
+
+with open("files/output/metrics.json", "w") as file:
+        file.write(json.dumps(metrics_train) + "\n")
+        file.write(json.dumps(metrics_test) + "\n")
+        file.write(json.dumps(c_train) + "\n")
+        file.write(json.dumps(c_test) + "\n")
